a02519783d19f4257ed97f3af5230a3be100bd42
CarterSos <carterjwalch@gmail.com>
2023-03-01 23:28:15 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_cjwalch.Models;
+using Mission09_cjwalch.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_cjwalch.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private BookstoreContext context { get; set; } // added in videos
+        ////public HomeController (WaterProjectContext temp)
+        ////{
+        ////    context = temp;
+        ////}
+
+        //public HomeController(BookstoreContext temp) => context = temp; // equivalent to same thing above (result is that context = temp) easy mode
+
+        // above is the old way using context
+
+        private IBookstoreRepository repo;
+
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1) // if nothing comes in then default to 1
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+    }
+}
---
+++ Models/IBookstoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_cjwalch.Models
+{
+    public interface IBookstoreRepository // interface is a template for a class that ensures it will be used correctly
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
